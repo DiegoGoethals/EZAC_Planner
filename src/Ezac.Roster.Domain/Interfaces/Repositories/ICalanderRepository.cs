c45b08087bb56efdfc7ea50ce8ce10e2105e66fb
Almanzo Rosseel <almanzo.rosseel@student.howest.be>
2024-05-06 10:59:21 +0200

    Let icalancerrepository interface inherit from ibaserepository

--- src/Ezac.Roster.Domain/Interfaces/Repositories/ICalanderRepository.cs
+++ src/Ezac.Roster.Domain/Interfaces/Repositories/ICalanderRepository.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Ezac.Roster.Domain.Entities;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,8 @@ using System.Threading.Tasks;
 
 namespace Ezac.Roster.Domain.Interfaces.Repositories
 {
-    public class ICalanderRepository
+    public interface ICalanderRepository : IBaseRepository<ApplicationCalendar>
     {
+
     }
 }
