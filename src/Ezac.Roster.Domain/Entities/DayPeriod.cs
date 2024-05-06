1c246ba1e72e9640a3b49dfead8c797d003a92df
DiegoGoethals <goethalsdiego@gmail.com>
2024-05-06 09:39:57 +0200

    Create day period entity #6

---
+++ src/Ezac.Roster.Domain/Entities/DayPeriod.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ezac.Roster.Domain.Entities
+{
+    public class DayPeriod : BaseEntity
+    {
+        public bool IsOpen { get; set; }
+        public DateTime Start {  get; set; }
+        public DateTime End { get; set; }
+        public Guid DayId { get; set; }
+        //public Day Day { get; set; }
+        //public ICollection<Preference> Preferences { get; set; }
+        //public ICollection<Job> Jobs {  get; set; } 
+    }
+}
