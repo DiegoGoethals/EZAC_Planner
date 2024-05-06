0d384561af3b99e03178d271f1a62a0daabedf5f
DiegoGoethals <goethalsdiego@gmail.com>
2024-05-06 14:11:49 +0200

    Add day repository #41

---
+++ Ezac.Roster.Infrastructure/Repositories/DayRepository.cs
@@ -0,0 +1,15 @@
+﻿using Ezac.Roster.Domain.Entities;
+using Ezac.Roster.Domain.Interfaces.Repositories;
+using Ezac.Roster.Infrastructure.Data;
+using Microsoft.Extensions.Logging;
+
+namespace Ezac.Roster.Infrastructure.Repositories
+{
+    public class DayRepository : BaseRepository<Day>, IDayRepository
+    {
+        public DayRepository(ApplicationDbContext applicationDbContext, ILogger<IBaseRepository<Day>> logger) 
+            : base(applicationDbContext, logger)
+        {
+        }
+    }
+}
