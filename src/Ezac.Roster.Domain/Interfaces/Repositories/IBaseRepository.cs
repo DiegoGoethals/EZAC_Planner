a259c3e04bd4a40a25bf0bec0a87b65806f2ad88
Mohammed Khalil <mohammed.khalil@student.howest.be>
2024-05-06 10:35:19 +0200

    Add IBaseRepository and make it generic and add methods in #20

---
+++ src/Ezac.Roster.Domain/Interfaces/Repositories/IBaseRepository.cs
@@ -0,0 +1,20 @@
+﻿using Ezac.Roster.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ezac.Roster.Domain.Interfaces.Repositories
+{
+    public interface IBaseRepository<T> where T : BaseEntity
+    {
+        Task<IEnumerable<T>> GetAllAsync();
+        Task<T> GetByIdAsync(int id);
+        IQueryable<T> GetAll();
+        Task<bool> AddAsync(T toAdd);
+        Task<bool> DeleteAsync(T toDelete);
+        Task<bool> UpdateAsync(T toUpdate);
+        Task<bool> CheckIfExistsAsync(int id);
+    }
+}
