21efbd3fc49c4562275cf08c466edd94d2c8ecfd
Almanzo Rosseel <almanzo.rosseel@student.howest.be>
2024-05-06 13:11:52 +0200

    Create ipreferenceservice

---
+++ src/Ezac.Roster.Domain/Interfaces/Services/IPreferenceService.cs
@@ -0,0 +1,19 @@
+﻿using Ezac.Roster.Domain.Entities;
+using Ezac.Roster.Domain.Services.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ezac.Roster.Domain.Interfaces.Services
+{
+    public interface IPreferenceService
+    {
+        Task<ResultModel<IEnumerable<Preference>>> GetAllAsync();
+        Task<ResultModel<Preference>> GetByIdAsync(Guid id);
+        Task<ResultModel<Preference>> AddAsync(Preference preference);
+        Task<ResultModel<Preference>> DeleteAsync(Guid id);
+        Task<ResultModel<Preference>> UpdateAsync(Preference preference);
+    }
+}
