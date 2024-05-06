1adfc01cd3496926bb735fdcc6fb4e665e41c236
Almanzo Rosseel <almanzo.rosseel@student.howest.be>
2024-05-06 15:46:48 +0200

    Create applicationcalendarcreaterequestmodel

---
+++ src/Ezac.Roster.Domain/Services/Models/ApplicationCalendarCreateRequestModel.cs
@@ -0,0 +1,18 @@
+﻿using Ezac.Roster.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ezac.Roster.Domain.Services.Models
+{
+    public class ApplicationCalendarCreateRequestModel
+    {
+        public string Name { get; set; }
+        public DateTime Created { get; set; }
+        public IEnumerable<Day> Days { get; set; }
+        public DateTime Start { get; set; }
+        public DateTime End { get; set; }
+    }
+}
