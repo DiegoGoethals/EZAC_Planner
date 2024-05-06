88b25d893a94e5916716c46af75142726053efa4
Mitchell Kerselaers <mitchell.kerselaers@student.howest.be>
2024-05-06 10:33:23 +0200

    add permission entity #5

---
+++ src/Ezac.Roster.Domain/Entities/Permission.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ezac.Roster.Domain.Entities
+{
+	public class Permission : BaseEntity
+	{
+		//public ICollection<User> Users { get; set; }
+		public ICollection<Job> Jobs { get; set; }
+	}
+}
