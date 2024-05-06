83460a701bdd4187530943af5ff8ce1ed62c9634
Almanzo Rosseel <almanzo.rosseel@student.howest.be>
2024-05-06 09:41:14 +0200

    Create class user and edit properties #2

---
+++ src/Ezac.Roster.Domain/Entities/User.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ezac.Roster.Domain.Entities
+{
+    public class User
+    {
+        public string Email { get; set; }
+        public double Scaling { get; set; }
+        public bool IsAdmin { get; set; }
+        //public ICollection<Permission> Permissions { get; set; }
+        //public ICollection<Preference> Preferences { get; set; }
+    }
+}
