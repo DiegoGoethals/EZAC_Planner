f6c70a70ef672926b2d4e75eff0d48799aab9e9e
Mohammed Khalil <mohammed.khalil@student.howest.be>
2024-05-06 11:07:50 +0200

    Add IUserService interface and add methods in #17

---
+++ src/Ezac.Roster.Domain/Interfaces/Services/IUserService.cs
@@ -0,0 +1,20 @@
+﻿using Ezac.Roster.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static Microsoft.EntityFrameworkCore.DbLoggerCategory;
+
+namespace Ezac.Roster.Domain.Interfaces.Services
+{
+    public interface IUserService
+    {
+        IEnumerable<User> GetAll();
+        User GetById(int id);
+        void Add(User addUser);
+        void Delete(int id);
+        void Update(User updateUser);
+        IEnumerable<User> SearchByName(string userName);
+    }
+}
