5f7019d0feaea9fad38f1e703671bbaa048c1437
Almanzo Rosseel <almanzo.rosseel@student.howest.be>
2024-05-06 15:47:06 +0200

    Create applicationcalendarupdaterequestmodel

---
+++ src/Ezac.Roster.Domain/Services/Models/ApplicationCalendarUpdateRequestModel.cs
@@ -0,0 +1,19 @@
+﻿using Ezac.Roster.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ezac.Roster.Domain.Services.Models
+{
+    public class ApplicationCalendarUpdateRequestModel
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public DateTime Updated { get; set; }
+        public ICollection<Day> Days { get; set; }
+        public DateTime Start { get; set; }
+        public DateTime End { get; set; }
+    }
+}
