ebc65d41cdf32386912d3c3973b26f7df0b6461e
Mohammed Khalil <mohammed.khalil@student.howest.be>
2024-05-06 09:35:44 +0200

    Add job entity and add properties in #4

---
+++ src/Ezac.Roster.Domain/Entities/Job.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ezac.Roster.Domain.Entities
+{
+    public class Job : BaseEntity
+    {
+        public double Weight { get; set; }
+        //public ICollection<User> Users { get; set; }
+        //public ICollection<Dayperiod> Dayperiods { get; set; }
+        //public ICollection<Preference> Preferences { get; set; }
+        //public ICollection<Permission> Permissions { get; set; }
+    }
+}
