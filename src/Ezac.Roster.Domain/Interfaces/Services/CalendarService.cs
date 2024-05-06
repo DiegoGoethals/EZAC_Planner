693c61c2a7346100e55eb12d692bac7db24e9374
Almanzo Rosseel <almanzo.rosseel@student.howest.be>
2024-05-06 11:43:59 +0200

    Create calendarservice class and inherit from icalendarservice

---
+++ src/Ezac.Roster.Domain/Interfaces/Services/CalendarService.cs
@@ -0,0 +1,32 @@
+﻿using Ezac.Roster.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ezac.Roster.Domain.Interfaces.Services
+{
+    public class CalendarService : ICalendarService
+    {
+        public Task<ApplicationCalendar> AddAsync(ApplicationCalendar calendar)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> DeleteAsync(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<ApplicationCalendar> GetByIdAsync(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> UpdateAsync(ApplicationCalendar calendar)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
