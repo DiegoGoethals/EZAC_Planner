2af131dcc69f621bce9e29f98c0922f17ce6a752
Almanzo Rosseel <almanzo.rosseel@student.howest.be>
2024-05-06 13:28:17 +0200

    Create calendarservice

--- src/Ezac.Roster.Domain/Services/CalendarService.cs
+++ src/Ezac.Roster.Domain/Services/CalendarService.cs
@@ -1,11 +1,12 @@
 ﻿using Ezac.Roster.Domain.Entities;
+using Ezac.Roster.Domain.Interfaces.Services;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Ezac.Roster.Domain.Interfaces.Services
+namespace Ezac.Roster.Domain.Services
 {
     public class CalendarService : ICalendarService
     {
