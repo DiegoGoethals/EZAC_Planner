8160b73e645bccb87c4646007922cdce1c8a7168
DiegoGoethals <goethalsdiego@gmail.com>
2024-05-06 14:19:27 +0200

    Add calendar repository #44

---
+++ Ezac.Roster.Infrastructure/Repositories/CalendarRepository.cs
@@ -0,0 +1,21 @@
+﻿using Ezac.Roster.Domain.Entities;
+using Ezac.Roster.Domain.Interfaces.Repositories;
+using Ezac.Roster.Infrastructure.Data;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ezac.Roster.Infrastructure.Repositories
+{
+    public class CalendarRepository : BaseRepository<ApplicationCalendar>, ICalanderRepository
+    {
+        public CalendarRepository(ApplicationDbContext applicationDbContext, ILogger<IBaseRepository<ApplicationCalendar>> logger) 
+            : base(applicationDbContext, logger)
+        {
+        }
+    }
+}
