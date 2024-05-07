61160d38b9b80e156d307886ce1b32e56cf0b757
Almanzo Rosseel <almanzo.rosseel@student.howest.be>
2024-05-07 09:14:11 +0200

    Create class dayperiodservice #36

---
+++ src/Ezac.Roster.Domain/Services/DayPeriodService.cs
@@ -0,0 +1,13 @@
+﻿using Ezac.Roster.Domain.Interfaces.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ezac.Roster.Domain.Services
+{
+    public class DayPeriodService 
+    {
+    }
+}
