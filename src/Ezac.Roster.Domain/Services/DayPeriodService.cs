ec4ecf6e20ab5930f92c97c2331ce5e976989dd2
Almanzo Rosseel <almanzo.rosseel@student.howest.be>
2024-05-07 09:15:09 +0200

    Impliment interface idayperiodservice in class dayperiodservice #36

--- src/Ezac.Roster.Domain/Services/DayPeriodService.cs
+++ src/Ezac.Roster.Domain/Services/DayPeriodService.cs
@@ -1,4 +1,6 @@
-﻿using Ezac.Roster.Domain.Interfaces.Services;
+﻿using Ezac.Roster.Domain.Entities;
+using Ezac.Roster.Domain.Interfaces.Services;
+using Ezac.Roster.Domain.Services.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -7,7 +9,26 @@ using System.Threading.Tasks;
 
 namespace Ezac.Roster.Domain.Services
 {
-    public class DayPeriodService 
+    public class DayPeriodService : IDayPeriodService
     {
+        public Task<ResultModel<DayPeriod>> AddAsync(DayPeriod dayPeriod)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<ResultModel<bool>> DeleteAsync(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<ResultModel<DayPeriod>> GetByIdAsync(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<ResultModel<bool>> UpdateAsync(DayPeriod dayPeriod)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
