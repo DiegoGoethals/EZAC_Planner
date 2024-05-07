81d3f9882822cc4187654f9786daf3333212f778
Almanzo Rosseel <almanzo.rosseel@student.howest.be>
2024-05-07 09:05:30 +0200

    Change type of property days to ienumerable #46

--- src/Ezac.Roster.Domain/Services/Models/ApplicationCalendarUpdateRequestModel.cs
+++ src/Ezac.Roster.Domain/Services/Models/ApplicationCalendarUpdateRequestModel.cs
@@ -12,7 +12,7 @@ namespace Ezac.Roster.Domain.Services.Models
         public Guid Id { get; set; }
         public string Name { get; set; }
         public DateTime Updated { get; set; }
-        public ICollection<Day> Days { get; set; }
+        public IEnumerable<Day> Days { get; set; }
         public DateTime Start { get; set; }
         public DateTime End { get; set; }
     }
