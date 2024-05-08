0bbc874f56ad031553c72b7640f05a60e3db2d08
Almanzo Rosseel <almanzo.rosseel@student.howest.be>
2024-05-08 14:09:37 +0200

    Create userlistmodel and edit properties #24

---
+++ src/Ezac.Roster.Domain/Models/UserListModel.cs
@@ -0,0 +1,14 @@
+﻿using Ezac.Roster.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ezac.Roster.Domain.Models
+{
+    public class UserListModel : BaseListModel
+    {
+        public IEnumerable<Permission> Permissions { get; set; }
+    }
+}
