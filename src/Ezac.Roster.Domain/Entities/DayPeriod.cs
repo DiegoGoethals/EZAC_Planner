c79792844db6551fea12688ad5cf62981109dfd9
Mitchell Kerselaers <mitchell.kerselaers@student.howest.be>
2024-05-14 11:13:54 +0200

    Inject permissionService into calendar.razor #116

--- src/Ezac.Roster.Domain/Entities/DayPeriod.cs
+++ src/Ezac.Roster.Domain/Entities/DayPeriod.cs
@@ -9,11 +9,14 @@ namespace Ezac.Roster.Domain.Entities
     public class DayPeriod : BaseEntity
     {
         public bool IsOpen { get; set; }
-        public DateTime Start {  get; set; }
+        public DateTime Start { get; set; }
         public DateTime End { get; set; }
         public Guid DayId { get; set; }
         public Day Day { get; set; }
         public ICollection<Preference> Preferences { get; set; }
-        public ICollection<Job>? Jobs {  get; set; } 
+        public ICollection<Job>? Jobs { get; set; }
+
+
+
     }
 }
