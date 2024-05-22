1097266a9120f687e42ea988852bf5e5dd588c5e
Mohammed Khalil <mohammed.khalil@student.howest.be>
2024-05-22 14:42:19 +0200

    Add export calendar to excel method in export service interface #149

--- src/Ezac.Roster.Domain/Interfaces/Services/IExportService.cs
+++ src/Ezac.Roster.Domain/Interfaces/Services/IExportService.cs
@@ -1,12 +1,14 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Ezac.Roster.Domain.Interfaces.Services
 {
-    internal interface IExportService
+    public interface IExportService
     {
+        Task ExportCalendarToExcel(Calendar calendar, string filePath);
     }
 }
