0782bc7c841bda1c6841e63ad6b510a8c83e9344
DiegoGoethals <goethalsdiego@gmail.com>
2024-05-23 09:34:15 +0200

    Set checkboxes same value as preference values #120

--- Ezac.Roster.Infrastructure/Repositories/DayPeriodRepository.cs
+++ Ezac.Roster.Infrastructure/Repositories/DayPeriodRepository.cs
@@ -1,6 +1,7 @@
 ﻿using Ezac.Roster.Domain.Entities;
 using Ezac.Roster.Domain.Interfaces.Repositories;
 using Ezac.Roster.Infrastructure.Data;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Logging;
 
 namespace Ezac.Roster.Infrastructure.Repositories
@@ -10,6 +11,16 @@ namespace Ezac.Roster.Infrastructure.Repositories
         public DayPeriodRepository(ApplicationDbContext applicationDbContext, ILogger<IBaseRepository<DayPeriod>> logger)
             : base(applicationDbContext, logger)
         {
+        }
+
+        public override async Task<DayPeriod> GetByIdAsync(Guid id)
+        {
+            return await _table.Include(dp => dp.Preferences).FirstOrDefaultAsync(dp => dp.Id == id);
+        }
+
+        public override async Task<IEnumerable<DayPeriod>> GetAllAsync()
+        {
+            return await _table.Include(dp => dp.Preferences).ToListAsync();
         }
     }
 }
