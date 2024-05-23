12f27666d5cf0d17eddd322558d791a32b9e4f36
Mitchell Kerselaers <mitchell.kerselaers@student.howest.be>
2024-05-23 11:26:54 +0200

    Implement getjobsbycalendaridasync method #153

--- Ezac.Roster.Infrastructure/Repositories/JobRepository.cs
+++ Ezac.Roster.Infrastructure/Repositories/JobRepository.cs
@@ -1,6 +1,7 @@
 ﻿using Ezac.Roster.Domain.Entities;
 using Ezac.Roster.Domain.Interfaces.Repositories;
 using Ezac.Roster.Infrastructure.Data;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
@@ -15,6 +16,17 @@ namespace Ezac.Roster.Infrastructure.Repositories
 		public JobRepository(ApplicationDbContext applicationDbContext, ILogger<IBaseRepository<Job>> logger)
 			: base(applicationDbContext, logger)
 		{
+		}
+
+		public async Task<List<Job>> GetJobsByCalendarIdAsync(Guid calendarId)
+		{
+			var jobs = await _applicationDbContext.Jobs
+			.Include(j => j.DayPeriod)
+			.ThenInclude(dp => dp.Day)
+			.Where(j => j.DayPeriod.Day.CalendarId == calendarId)
+			.ToListAsync();
+
+			return jobs;
 		}
 	}
 }
